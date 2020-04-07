6bcdf2e9251f1743a52308296e0209394682d1a3
pavelcahn <pavel.cahn@gmail.com>
2020-04-07 15:32:17 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from "prop-types"
+
+/**
+ * Default breakpoint
+ * @type {Number}
+ */
+const WINDOW_BREAKPOINT = 768;
+
+/**
+ * Pass viewport info to Child component
+ * Update on resize evet
+ * @param  {Component} Child 
+ * @return {Component}
+ */
+export default (Child) => {
+
+	/**
+	 * Returned component
+	 * @param {Number} breakpoint Mobile Breakpoint, default to 768
+	 */
+	function Component(props) {
+		const [ screenState, setScreenState ] = useState({
+			device: 'mobile', 
+			docWidth: 320, 
+			docHeight: 480
+		});
+
+		useEffect(() => {
+			/**
+			 * Update window info on resize
+			 */
+			function onWindowResize() {
+				const docWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+				const docHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+				const device = window.innerWidth > props.breakpoint ? 'desktop' : 'mobile'
+				
+				setScreenState({device, docWidth, docHeight});
+			}
+			
+			onWindowResize();
+			window.addEventListener('resize', onWindowResize);
+			return () => {
+				window.removeEventListener('resize', onWindowResize);
+			}
+		}, [screenState.device]);
+
+		return <Child { ...props } screenState = { screenState } />
+	}
+
+	Component.defaultProps = {
+		breakpoint: WINDOW_BREAKPOINT
+	}
+
+	Component.propTypes = {
+		breakpoint: PropTypes.number
+	}
+
+	return Component;
+}
